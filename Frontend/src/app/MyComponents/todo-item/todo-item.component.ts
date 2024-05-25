d04d7a8e602ed9c3dc903bb589f85a6f928388c8
Aditya Navgare <adityanavgare162@gmail.com>
2024-05-26 02:03:36 +0530

    frontend add

---
+++ Frontend/src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from '../../Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.css'
+})
+
+export class TodoItemComponent implements OnInit
+{
+  @Input() todo!: Todo;
+  @Input() i!: number;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
+
+
+  constructor()
+  {
+
+  }
+
+  ngOnInit(): void 
+  {
+
+  }
+
+  onClick(todo:Todo)
+  {
+    this.todoDelete.emit(todo);
+    console.log("Onclick function has been triggered ")
+  }
+
+  onCheckboxClick(todo: any)
+  {
+    this.todoCheckbox.emit(todo);
+  }
+
+}
